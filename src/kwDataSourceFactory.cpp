23f1857c588ee6a49234807f90968b6928e33c51
Christoph Uhlich <christoph@familie-uhlich.de>
2018-02-08 15:53:31 -0500

    Corrected compare call and json submodule path.

--- src/kwDataSourceFactory.cpp
+++ src/kwDataSourceFactory.cpp
@@ -23,10 +23,6 @@ kwDataSourceFactory::~kwDataSourceFactory()
 
 shared_ptr<kwObject> kwDataSourceFactory::createObject(kwValString* classname, json configuration)
 {
-//	if (classname->str()->compare("kwShm") == 0)
-//		return new kwShm(configuration);
-//	if (classname->str()->compare("kwSocketClient") == 0)
-//		return new kwSocketClient(configuration);
 	if (classname->compare("kwDataContainer") == 0)
 		return shared_ptr<kwDataContainer>(new kwDataContainer(configuration));
 
