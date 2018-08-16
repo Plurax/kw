3381953de893911f82df180b7a942cea344bc193
Christoph Uhlich <christoph@familie-uhlich.de>
2018-08-16 21:59:31 +0200

    Coverage info generation added.

--- test/src/unit-kwValString.cpp
+++ test/src/unit-kwValString.cpp
@@ -10,16 +10,34 @@ TEST_CASE( "kwValString Method tests - should behave the same like string", "[kw
   kwValString string_B = kwValString("TestB");
 
   string str_string_A = string("TestA");
-  string str_string_B = string("TestA");
+  string str_string_B = string("TestB");
 
   REQUIRE( str_string_A == string_A.str());
+  REQUIRE( str_string_B == string_B.str());
 
-  // SECTION( "resizing bigger changes size and capacity" ) {
-  // }
-  // SECTION( "resizing smaller changes size but not capacity" ) {
-  // }
-  // SECTION( "reserving bigger changes capacity but not size" ) {
-  // }
-  // SECTION( "reserving smaller does not change size or capacity" ) {
-  // }
+  char* nonConstCStr = "NonConstcstr";
+  string nonConstStr = string("NonConstcstr");
+
+  SECTION( "Calling base constructor will cause empty true" ) {
+    REQUIRE( string_A.empty() == false);
+    kwValString emptyString = kwValString();
+    REQUIRE( emptyString.empty() == true);
+  }
+
+  SECTION( "Non const C String construtors" ) {
+    kwValString nonConstkwStr = kwValString(nonConstCStr);
+    REQUIRE( nonConstkwStr.str() == nonConstStr);
+    REQUIRE( nonConstkwStr.empty() == false);
+  }
+
+  SECTION( "Copy constructor" ) {
+    kwValString copy = string_A;
+    REQUIRE( str_string_A == copy.str());
+  }
+
+  SECTION( "Operator+" ) {
+    kwValString added = string_A + string_B;
+    string str_add = str_string_A + str_string_B;
+    REQUIRE( str_add == added.str());
+  }
 }
