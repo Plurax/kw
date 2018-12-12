af8a25a9b44b1ee67c955d79c5c599a1a8b485f5
Christoph Uhlich <christoph@familie-uhlich.de>
2018-12-12 19:48:08 +0100

    Increased test coverage...

--- test/src/unit-kwValFloat.cpp
+++ test/src/unit-kwValFloat.cpp
@@ -1,17 +1,20 @@
 #include "catch.hpp"
 #include "kw/kwValFloat.h"
+#include "kw/kwValString.h"
 #include "string.h"
 #include <sstream>
+#include <memory>
+#include <limits>
 
 using namespace std;
 
 TEST_CASE( "kwValFloat Method tests - should behave the same like float", "[kwValFloat]" ) {
 
     float fl = 1.5;
-    kwValFloat val1 = -1000.5;
-    kwValFloat val2 = -1.5;
-    kwValFloat val3 = 1.5;
-    kwValFloat val4 = 1000.2;
+    kwValFloat val1 = -1000.5f;
+    kwValFloat val2 = -1.5f;
+    kwValFloat val3 = 1.5f;
+    kwValFloat val4 = 1000.2f;
 
     SECTION("Constructor") {
         kwValFloat val;
@@ -38,10 +41,55 @@ TEST_CASE( "kwValFloat Method tests - should behave the same like float", "[kwVa
         REQUIRE( (val1 <= val2) == true);
     }
 
-    SECTION("Calculations") {
-        kwValFloat res = val1 + val2;
-        REQUIRE( res == kwValFloat(-1002));
+    SECTION("asString") {
+        // This only tests the standard behaviour, to create custom
+        // formatting you need to use stringstream instead.
+        REQUIRE( kwValString("-1.500000") == val2.asString());
+        REQUIRE( kwValString("1.500000") == val3.asString());
+    }
 
-        kwValFloat res2 = val1 + val4;
+    SECTION("Calculations") {
+        kwValFloat sum1 = val1 + val2;
+        REQUIRE( sum1 == kwValFloat(-1002));
+
+        kwValFloat sum2 = val1 + val4;
+        REQUIRE( sum2.getValue() == Approx(-0.3f).epsilon(0.0001));
+
+        // shortened calculations
+        kwValFloat res3 = kwValFloat(500.5);
+        res3 += val1;
+        REQUIRE( res3 == kwValFloat(-500));
+
+        kwValFloat difference1 = res3 - val4;
+        REQUIRE( difference1 == kwValFloat(-1500.2));
+
+        kwValFloat difference2 = kwValFloat(1000);
+        kwValFloat subtrahend = kwValFloat(1000);
+        difference2 -= subtrahend;
+        REQUIRE(difference2  == kwValFloat(0));
+
+        kwValFloat factor1 = kwValFloat(10.1);
+        kwValFloat factor2 = kwValFloat(6);
+        kwValFloat product = factor1 * factor2;
+        REQUIRE(product.getValue()  == Approx(kwValFloat(60.6).getValue()));
+
+        kwValFloat divident = kwValFloat(10.2);
+        kwValFloat divisor = kwValFloat(2);
+        kwValFloat quotient = divident / divisor;
+        REQUIRE(quotient  == kwValFloat(5.1));
+    }
+
+    SECTION("Pointer type destructor branch") {
+        kwValFloat* rawptr = new kwValFloat(0.5);
+        kwValFloat rawptrres = kwValFloat(0.3) + *rawptr;
+        delete rawptr;
+        REQUIRE(rawptrres.getValue() == Approx(0.8));
+    }
+
+    SECTION("json serialization") {
+        json expected = { {"float", -1000.5}};
+        json generated;
+        generated["float"] = val1.toJson();
+        REQUIRE(expected == generated);
     }
 }
--- test/src/unit-kwValInteger.cpp
+++ test/src/unit-kwValInteger.cpp
@@ -2,36 +2,93 @@
 #include "kw/kwValInteger.h"
 #include "string.h"
 #include <sstream>
+#include <memory>
 
 using namespace std;
 
 TEST_CASE( "kwValInteger Method tests - should behave the same like string", "[kwValString]" ) {
 
-  int i = 1;
-  kwValInteger val1 = -1000;
-  kwValInteger val2 = -1;
-  kwValInteger val3 = 1;
-  kwValInteger val4 = 1000;
+    int i = 1;
+    kwValInteger val1 = -1000;
+    kwValInteger val2 = -1;
+    kwValInteger val3 = 1;
+    kwValInteger val4 = 1000;
 
-  SECTION("Constructor") {
-    kwValInteger val;
-    REQUIRE(val.getValue() == 0);
-  }
+    SECTION("Constructor") {
+        kwValInteger val;
+        REQUIRE(val.getValue() == 0);
+    }
 
-  SECTION( "O Stream") {
-    std::ostringstream os;
-    os << val1;
-    string fromOS = os.str();
-    REQUIRE(fromOS == string("-1000"));
-  }
+    SECTION( "O Stream") {
+        std::ostringstream os;
+        os << val1;
+        string fromOS = os.str();
+        REQUIRE(fromOS == string("-1000"));
+    }
 
-  SECTION("Comparisons") {
-    REQUIRE( (val1 > val2) == false);
-    REQUIRE( (val1 < val2) == true);
-    kwValInteger clone = val1;
-    REQUIRE( (clone >= val1) == true);
-    REQUIRE( (clone <= val1) == true);
-    REQUIRE( (val1 >= val2) == false);
-    REQUIRE( (val1 <= val2) == true);
-  }
+    SECTION("Comparisons") {
+        REQUIRE( (val1 > val2) == false);
+        REQUIRE( (val1 < val2) == true);
+        kwValInteger clone = val1;
+        REQUIRE( (clone >= val1) == true);
+        REQUIRE( (clone <= val1) == true);
+        REQUIRE( (val1 >= val2) == false);
+        REQUIRE( (val1 <= val2) == true);
+    }
+
+    SECTION("Assignments") {
+        kwValInteger integerassign = 50;
+        REQUIRE(integerassign.getValue() == 50);
+    }
+
+    SECTION("asString") {
+        // This only tests the standard behaviour, to create custom
+        // formatting you need to use stringstream instead.
+        REQUIRE( kwValString("-1000") == val1.asString());
+        REQUIRE( kwValString("1000") == val4.asString());
+    }
+
+    SECTION("Calculations") {
+        // Assume the same behaviour like integers
+        kwValInteger sum = val1 + val4;
+        REQUIRE(sum.getValue() == 0);
+
+        kwValInteger sum2 = kwValInteger(500);
+        sum2 += val1;
+        REQUIRE(sum2.getValue() == -500);
+
+        kwValInteger difference1 = val4 - val3;
+        REQUIRE(difference1.getValue() == 999);
+
+        kwValInteger difference2 = kwValInteger(500);
+        difference2 -= val4;
+        REQUIRE(difference2.getValue() == -500);
+
+        kwValInteger product = val1 * kwValInteger(5);
+        REQUIRE(product.getValue() == -5000);
+
+        kwValInteger quotient = val4 / kwValInteger(5);
+        REQUIRE(quotient.getValue() == 200);
+    }
+
+    SECTION("Assignment") {
+        std::shared_ptr<kwValInteger> val5 = std::make_shared<kwValInteger>(50);
+        REQUIRE(val5->getValue() == 50);
+        kwValInteger val6 = 60;
+        REQUIRE(val6.getValue() == 60);
+    }
+
+    SECTION("Pointer type destructor branch") {
+        kwValInteger* rawptr = new kwValInteger(5);
+        kwValInteger rawptrres = kwValInteger(3) + *rawptr;
+        delete rawptr;
+        REQUIRE(rawptrres.getValue() == 8);
+    }
+
+    SECTION("json serialization") {
+        json expected = { {"int", -1000}};
+        json generated;
+        generated["int"] = val1.toJson();
+        REQUIRE(expected == generated);
+    }
 }
